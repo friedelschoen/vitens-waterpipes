ae6b30c2454c9c49ee8ad8a0bc49da66ca995f96
MineRianne <83002034+MineRianne@users.noreply.github.com>
2025-06-13 16:38:43 +0200

    wow new backend

---
+++ backend/backend/sensor_logger_runner.py
@@ -0,0 +1,17 @@
+# sensor_logger_runner.py
+
+import time
+from SensorDataLogger import DummyFlowSensorLogger
+
+logger = DummyFlowSensorLogger()
+
+# Demo loop — prints mock data every 2 seconds
+if __name__ == "__main__":
+    try:
+        while True:
+            data = logger.read_sensors()
+            print(data)
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("Stopped mock sensor logger.")
+
